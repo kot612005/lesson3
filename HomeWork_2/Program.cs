7a7efd8d07474cd34321c7480094fe7d1ef8d98f
ekaterinashliapnikova <kot612005@yandex.ru>
2022-06-25 22:25:46 +0700

    homework2

---
+++ HomeWork_2/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max;
+if (a>b) max=a;
+else max=b;
+if (c>max) max=c;
+Console.Write (max);
