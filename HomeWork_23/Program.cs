94ac093329236ef123b9cb3b42fe8f51a6dd2af8
ekaterinashliapnikova <kot612005@yandex.ru>
2022-07-02 14:54:34 +0700

    дополнительная задачка

---
+++ HomeWork_23/Program.cs
@@ -0,0 +1,8 @@
+﻿int k;
+string w;
+Console.WriteLine("определить K \n ");
+k = Convert.ToInt16(Console.ReadLine());
+w = Convert.ToString(k, 2);
+w = w.Replace('1', '5');
+Console.WriteLine("К-е по порядку круглое число \n ");
+Console.WriteLine(w);
