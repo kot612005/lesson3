c2aee333dd35695e7ed20f0c1e1a1bf1958b2085
ekaterinashliapnikova <kot612005@yandex.ru>
2022-07-02 13:48:54 +0700

    Homework19

---
+++ HomeWork_19/Program.cs
@@ -0,0 +1,9 @@
+﻿int a, b, c, d, e;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+d = Convert.ToInt32(Console.ReadLine());
+e = Convert.ToInt32(Console.ReadLine());
+if( (a==e) && (b == d) )
+Console.WriteLine("Полиндром");
+else Console.WriteLine("Нет");
