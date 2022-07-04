c714fe70754f5a0b2913c8b17efd11314a201722
ekaterinashliapnikova <kot612005@yandex.ru>
2022-07-04 20:30:12 +0700

    задача 25

---
+++ HomeWork_25/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a;
+int count = 1;
+while(count < b)
+{
+    result = result * a;
+    count++;
+}
+Console.WriteLine(result);
