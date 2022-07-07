9b226742a5b7b284e66375f93506a632ec1bc77d
ekaterinashliapnikova <kot612005@yandex.ru>
2022-07-07 18:57:28 +0300

    Задача 36

---
+++ HomeWork_36/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int[7];
+int sum  = 0;
+for (int i = 0; i < 7; i++)
+{
+   array[i] = new Random().Next(1, 100); 
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+for (int i = 0; i < 7; i= i+2)
+    {
+       sum = sum + array[i];
+    }
+
+Console.WriteLine(sum);
+
