6706309fc01e27f6271fe6d21e20c885fb6e41aa
ekaterinashliapnikova <kot612005@yandex.ru>
2022-07-07 18:28:42 +0300

    задача 34

---
+++ HomeWork_34/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int[6];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 999);
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+   if  (array[i] % 2 == 0)
+   count++;
+  
+}
+Console.WriteLine(count);
