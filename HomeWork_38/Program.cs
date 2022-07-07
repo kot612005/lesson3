29bf07d9dfcc910d004aaafd4c57e98613741757
ekaterinashliapnikova <kot612005@yandex.ru>
2022-07-07 19:58:09 +0300

    задача 38

---
+++ HomeWork_38/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = new int[7];
+int sum  = 0;
+int max = 0;
+int min = 100;
+for (int i = 0; i < 7; i++)
+{
+   array[i] = new Random().Next(1, 100); 
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+for (int i = 0; i < 7; i++)
+{
+   if (array [i] > max)
+    max = array [i]; 
+}
+Console.WriteLine (max);
+for (int i = 0; i < 7; i++)
+{
+   if (min > array[i])
+    min = array [i]; 
+}
+Console.WriteLine (min);
+Console.WriteLine ("Разница максимального и минимального: "+ (max-min));
