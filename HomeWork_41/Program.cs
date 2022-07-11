e26e80eecddbd4041b68368fc7bb08fdc7559eec
ekaterinashliapnikova <kot612005@yandex.ru>
2022-07-11 13:38:06 +0300

    Задача 41

---
+++ HomeWork_41/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите 6 чисел: ");
+int[] array = new int[6];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+   if  (array[i] > 0)
+   count++;
+}
+Console.WriteLine(count);
