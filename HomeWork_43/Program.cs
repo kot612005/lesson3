4fe163d3c862c759184ba2c4e0ace9892fa18b4c
ekaterinashliapnikova <kot612005@yandex.ru>
2022-07-11 14:33:35 +0300

    Задача 43

---
+++ HomeWork_43/Program.cs
@@ -0,0 +1,12 @@
+﻿double b1, b2, k1, k2, x, y;
+Console.Write("Введите значения: ");
+b1 = Convert.ToDouble(Console.ReadLine());
+b2 = Convert.ToDouble(Console.ReadLine());
+k1 = Convert.ToDouble(Console.ReadLine());
+k2 = Convert.ToDouble(Console.ReadLine());
+x = (b1-b2)/(k2-k1);
+y = k1 * x + b1;
+
+Console.WriteLine("точка пересечения двух прямых = ");
+Console.WriteLine(x);
+Console.WriteLine(y);
